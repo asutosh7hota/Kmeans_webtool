2fb6cf02812f114612e704a6b33bb94d79d3cdd7
zyaj <zyaj@stanford.edu>
2017-09-16 22:20:16 -0700

    complete version 1

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
 from flask import Flask, render_template, request, redirect
+import numpy as np
 import io
 import csv
-import numpy as np
 import sys
 import kmeans as kn
 app = Flask(__name__)
+# store global variable
+app.vars={}
 
 @app.route('/')
 def main():
-	return redirect('/index')
+	return redirect('/k-means')
 
-@app.route('/index',methods=['GET'])
-def index():
+# first page for input
+@app.route('/k-means',methods=['GET'])
+def k_means():
    # page for upload csv data and number of cluster
-   return render_template('index.html')
+   return render_template('k-means.html')
 	
 @app.route('/result', methods = ['POST'])
 def result():
    if request.method == 'POST':
       # ------------------------
       # load in input data
+      # ------------------------
       # if error, output error.html
       # get number of cluster
       try:
          c_cnt=int(request.form['c_cnt'])
+      except ValueError:
+         error_msg='Please provide the number of clusters'
+         return render_template('error.html',error_msg=error_msg)
+      
+      # set default value for method and n_init if no value is set.
+      try:
          method=int(request.form['method'])
          n_init=int(request.form['n_init'])
-
       except ValueError:
-         error_msg='Wrong input for the number of clusters, initialization method or number of runs.'
-         return render_template('error.html',error_msg=error_msg)
+         method=2
+         n_init=30
 
       # get the vector data
       csvfile = io.TextIOWrapper(request.files['file'].stream, encoding='gbk')
@@ -46,11 +55,13 @@ def result():
 
       # -----------------
       # clustering all X
+      # -----------------
       kmeans=kn.KMeans(c_cnt,method=method,n_init=n_init)
       kmeans.fit(X)
 
       # ------------------
       # output results
+      # ------------------
       # the number of nodes for each cluster
       c_node_num=np.array([len(nodes) for nodes in kmeans.clusters.nodes])
       
@@ -58,13 +69,27 @@ def result():
       centers=np.array([x for _, x in sorted(zip(c_node_num,kmeans.clusters.coords), key=lambda pair: pair[0])])
       centers=np.round(centers,1)
       centers=[', '.join(center.astype(str)) for center in centers]
+
+      # save detailed clustering result
+      app.vars['result']=[x for _, x in sorted(zip(c_node_num,kmeans.clusters.nodes), key=lambda pair: pair[0])]
+      app.vars['result']=[[str(node) for node in cluster] for cluster in app.vars['result']]
+
       # sorted node number for each cluster
       c_node_num=','.join(sorted(c_node_num.astype(str)))
+      
       return render_template('result.html',
          c_cnt=c_cnt,c_node_num=c_node_num,centers=centers,node_num=X.shape[0])
-		
+
+# store clustering results
+@app.route("/more_result")
+def data():
+   return render_template('more_result.html',result=[', '.join(cluster) for cluster in app.vars['result']])
+
 if __name__ == '__main__':
    try:
-      app.run(debug = True,port=int(sys.argv[1]))
+      if len(sys.argv)>1:
+         app.run(debug = True,port=int(sys.argv[1]))
+      else:
+         app.run(debug = True,port=5000)
    except PermissionError:
       print('Permission Error! Try different port number, maybe >5000')
